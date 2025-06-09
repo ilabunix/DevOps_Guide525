78ac50185caa0a8b740b3c19d9755962a25dbc31
AWSCloudEng <79238784+ilabunix@users.noreply.github.com>
2025-06-09 11:39:56 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+CONFLUENCE_URL = "https://<your-subdomain>.atlassian.net/wiki"
+CONFLUENCE_EMAIL = "your-email@example.com"
+CONFLUENCE_API_TOKEN = "your-api-token"
+CONFLUENCE_SPACE_KEY = "TEST"
+
+OPENSEARCH_ENDPOINT = "https://<your-endpoint>.aos.<region>.amazonaws.com"
+OPENSEARCH_INDEX = "confluence-pages"
