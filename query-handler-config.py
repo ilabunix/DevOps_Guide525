676e263c2849fd0bb45e8957ef5ea2ab0b164a8b
AWSCloudEng <79238784+ilabunix@users.noreply.github.com>
2025-06-12 11:54:48 -0500

    Create query-handler-config.py

---
+++ query-handler-config.py
@@ -0,0 +1,5 @@
+OPENSEARCH_ENDPOINT = "https://your-domain.us-west-2.es.amazonaws.com"
+OPENSEARCH_INDEX = "confluence-pages"
+
+OS_USERNAME = "your-admin-username"
+OS_PASSWORD = "your-admin-password"
